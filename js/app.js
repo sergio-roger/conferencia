a34fee069a87d80b24edaa26a6b422be2f494f5d
sergio-roger <sergio.lkg.31@gmail.com>
2019-07-05 17:58:43 -0500

    Arreglo de bug menu

--- js/app.js
+++ js/app.js
@@ -1,13 +1,53 @@
 $(document).ready(function() {
-    $('a[href^="#"]').click(function() {
-      var destino = $(this.hash);
-      if (destino.length == 0) {
-        destino = $('a[name="' + this.hash.substr(1) + '"]');
-      }
-      if (destino.length == 0) {
-        destino = $('html');
-      }
-      $('html, body').animate({ scrollTop: destino.offset().top }, 500);
-      return false;
+    var op = 0;
+
+    $(window).resize(function(){
+        var ancho = $(window).width();
+        // console.log(ancho);
+
+        if(ancho >= 768){
+            $('.menu').css({"transform":'translateX(0%)'});
+            op = 1;
+        }
     });
-  });
+
+    function Scroll(){
+        $('a[href^="#"]').click(function() {
+            var destino = $(this.hash);
+            if (destino.length == 0) {
+              destino = $('a[name="' + this.hash.substr(1) + '"]');
+            }
+            if (destino.length == 0) {
+              destino = $('html');
+            }
+            $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+            return false;
+          });
+    }
+
+    $('.id-menu').click(function(){
+        // console.log('ancla presionado')
+        Deslizmiento();        
+    });
+
+    function Deslizar_menu(){
+        $('#icono-menu').click(function(){
+            // transform: translateX(-100%);
+            Deslizmiento();
+        });
+    }
+    function Deslizmiento(){
+        if(op == 0){
+            $('.menu').css({"transform":'translateX(0%)'});
+            op = 1;
+        }
+        else{
+            $('.menu').css({"transform":'translateX(-100%)'});
+            op = 0;
+        }
+        // console.log('Var op = ', op);
+    }
+
+    Deslizar_menu();
+    Scroll();
+});
